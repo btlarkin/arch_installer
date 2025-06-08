22afc23b572390aef0a48a1905e2fcb6d72bd17d
btlarkin <149228586+btlarkin@users.noreply.github.com>
2025-06-08 15:44:22 -0500

    add bootloader grub

--- install_chroot.sh
+++ install_chroot.sh
@@ -6,4 +6,19 @@ uefi=$(cat /var_uefi); hd=$(cat /var_hd);
 # Naming the system
 cat /comp > /etc/hostname && rm /comp
 
+# The Bootloader GRUB
+pacman --noconfirm -S dialog
+pacman -S --noconfirm grub
+
+if [ "$uefi" = 1 ]; then
+    pacman -S --confirm efibootmgr
+    grub-install --target=x86_64-efi \
+        --bootloader-id=GRUB \
+        --efi-directory=/boot/efi
+else
+    grub-install "$hd"
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
 
