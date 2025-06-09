698101f44b4e8a1ed7c1afe0319bc7670b318708
btlarkin <149228586+btlarkin@users.noreply.github.com>
2025-06-09 03:32:38 -0500

    install the packages

--- install_apps.sh
+++ install_apps.sh
@@ -46,5 +46,33 @@ echo "$selection" "$lines" "$count" >> "/tmp/packages"
 # Updating system
 pacman -Syu --noconfirm
 
+# Installing the packages
+rm -f /tmp/aur_queue
 
+dialog --title "Let's go!" --msgbox \
+"The system will now install everything you need.\n\n\
+It will take some time.\n\n " \
+13 60
+
+c=0
+echo "$packages" | while read -r line; do
+    c=$(( "$c" + 1 ))
+
+    dialog --title "Arch Linux Installation" --infobox \
+    "Downloading and installing program $c out of $count: $line..." \
+    8 70
+
+    ((pacman --noconfirm --needed -S "$line" > /tmp/arch_install 2>&1) \
+    || echo "$line" >> /tmp/aur_queue) \
+    || echo "$line" >> /tmp/arch_install_failed
+
+    if [ "$line" = "zsh" ]; then
+        # Set Zsh as default terminal for our user
+        chsh -s "$(which zsh)" "$name"
+    fi
+
+    if [ "$line" = "networkmanager" ]; then
+        systemctl enable NetworkManager.service
+    fi
+done
 
