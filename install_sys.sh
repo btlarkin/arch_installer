ceaedda16b7f9ea4f5a6cfbb001718087bbd6dcb
The Lark <149228586+btlarkin@users.noreply.github.com>
2025-10-05 12:55:44 -0400

    docs: fix minor typos

--- install_sys.sh
+++ install_sys.sh
@@ -7,7 +7,7 @@ timedatectl set-ntp true
 
 # Welcome message of type yesno - see `man dialog`
 dialog --defaultno --title "Are you sure?" --yesno \
-"This is my personnal arch linux install. \n\n\
+"This is my personal arch linux install. \n\n\
 It will just DESTROY EVERYTHING on the hard disk of your choice. \n\n\
 Don't say YES if you are not sure about what you're doing! \n\n\
 Are you sure?" 15 60 || exit
